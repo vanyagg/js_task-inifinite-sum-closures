05b20969d1fefd29d15fe23ec15cfb6bc3d24827
Vanya Holovey <golovejvana9@gmail.com>
2025-06-10 23:20:52 +0300

    made infinity sum closures

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,25 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let total = 0;
+
+  const adder = (...values) => {
+    const sum = values.reduce((acc, value) => acc + value, 0);
+
+    total += sum;
+
+    if (values.length === 0) {
+      const result = total;
+
+      total = 0;
+
+      return result;
+    }
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
